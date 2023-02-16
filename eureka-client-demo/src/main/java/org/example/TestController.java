c7751217e2e0b63a758398137e1e864b9cefd59e
Eleven <idiotalex@163.com>
2023-02-17 00:04:39 +0800

    add eureka demo

---
+++ eureka-client-demo/src/main/java/org/example/TestController.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.sun.org.slf4j.internal.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Slf4j
+@RestController
+public class TestController {
+
+    @Autowired
+    private DiscoveryClient client;
+
+
+    @SuppressWarnings("deprecation")
+    @GetMapping("/info")
+    public String info() {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        String info = "host：" + instance.getHost() + "，service_id：" + instance.getServiceId();
+        log.info(info);
+        return info;
+    }
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
